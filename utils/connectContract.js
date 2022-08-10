9e93e87afcf12fb1ada14fd2766f81d6d3ec02d4
Idoia Rojo Lazaro <idoia@ifeelonline.com>
2022-08-10 09:32:41 +0200

    connect contract, add abi, create event function

---
+++ utils/connectContract.js
@@ -0,0 +1,27 @@
+import abiJSON from "../utils/Web3RSVP.json";
+import { ethers } from "ethers";
+
+function connectContract() {
+  const contractAddress = "0x6141aed1Cb002cfD27E726d9cFc4673462AD4CaC";
+  const contractABI = abiJSON.abi;
+  console.log("abi", "hola holi hello");
+  let rsvpContract;
+  try {
+    const { ethereum } = window;
+
+    if (ethereum.chainId === "0x13881") {
+      //checking for eth object in the window, see if they have wallet connected to Mumbai network
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      console.log("contractABI", contractABI);
+      rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
+    } else {
+      throw new Error("Please connect to the Polygon Mumbai network.");
+    }
+  } catch (error) {
+    console.log("ERROR:", error);
+  }
+  return rsvpContract;
+}
+
+export default connectContract;
